e1928b402cde76ec2ec783865906ffb9e9f1fb4e
Arun-Ganesh07 <74291980+Arun-Ganesh07@users.noreply.github.com>
2022-02-09 11:48:50 -0800

    Add files via upload

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router,Routes, Route, NavLink } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppointmentTable from './Components/AppointmentTable'
+import AppointmentForm from './Components/AppointmentForm'
+import axios from 'axios';
 
 function Dashboard() {
-    return ( 
-      <div className='container'> 
-         <Table />
-      </div> 
-    ); 
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    fetchAppointments().then(result => {
+      if(result)
+        setAppointments(result);
+    });
+  }, []);
+
+  async function fetchAppointments(){
+    try{
+      const response = await axios.get('http://localhost:5000/dashboard');
+      return response.data.appointmentData;
+    }catch(error){
+      console.log(error);
+      return false;
+    }
+  }
+
+    return (
+      <div>
+          <DashboardHeader appointmentData={appointments}/>
+      </div>
+  ); 
+}
+
+function DashboardHeader(props){
+  return (
+    <Router>
+      <Navbar bg='dark' variant='dark'  >
+        <Container>
+          <Navbar.Brand href="/">Dashboard</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/appointments">Appointments</Nav.Link>
+            {/* <Nav.Link href="/clients">Clients</Nav.Link>
+            <Nav.Link href="/dogs">Dogs</Nav.Link>
+            <Nav.Link href="/calendar">Calendar</Nav.Link> */}
+          </Nav>
+        </Container>
+      </Navbar >
+
+      <Routes>
+        <Route path='/' element ={<AppointmentTable appointmentData={props.appointmentData} />}/>
+        <Route path='/appointments' element={<AppointmentForm />} />
+      </Routes>
+
+    </Router>
+  );
 }
 
 export default Dashboard;
